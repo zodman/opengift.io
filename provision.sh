668a6216fe57f641bf544e4b426f5fa9692d23e4
Artem Smirnov <tonakai.personal@gmail.com>
2014-09-18 16:07:18 +0400

    Added provision reload mysql, nginx and use conf from supervisor/vagrant.conf for supervisor

--- provision.sh
+++ provision.sh
@@ -1,3 +1,6 @@
+cp /vagrant/supervisor/vagrant.conf /etc/supervisor/conf.d/heliard.conf
+supervisorctl reload
 python /vagrant/manage.py schemamigration PManager --auto
 python /vagrant/manage.py migrate PManager
-supervisorctl reload
+service nginx restart
+service mysql restart
