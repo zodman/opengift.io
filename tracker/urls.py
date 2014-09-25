82624edb17d9e95f8438a6310b5794170bed5857
Gvammer <gvammer@rambler.ru>
2014-09-25 20:08:11 +0400

    robots txt

--- tracker/urls.py
+++ tracker/urls.py
@@ -110,7 +110,7 @@ urlpatterns = patterns('',
 
                        # Uncomment the next line to enable the admin:
                        url(r'^admin/', include(admin.site.urls)),
-                       (r'^robots\.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /", mimetype="text/plain")),
+                       (r'^robots\.txt$', lambda r: HttpResponse("User-agent: *", mimetype="text/plain")),
                        # (r'^search/', include('haystack.urls')),
                        url(r'^robokassa/', include('robokassa.urls')),
                        url(r'^payment_info/', paysystems),
