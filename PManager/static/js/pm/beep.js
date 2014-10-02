470b099d089b26d70419b361ac860b526d805cb8
gvammer <gvamm3r@gmail.com>
2014-10-02 22:43:28 +0400

    knock and second metrika goal

--- PManager/static/js/pm/beep.js
+++ PManager/static/js/pm/beep.js
@@ -4,31 +4,15 @@
  * Date: 15.07.14
  * Time: 16:18
  */
-var beep = (function () {
-    var ctx = new(window.audioContext || window.webkitAudioContext);
-    if (!ctx) return false;
-    return function (duration, type, finishedCallback) {
-
-        duration = +duration;
-
-        // Only 0-4 are valid types.
-        type = (type % 5) || 0;
-
-        if (typeof finishedCallback != "function") {
-            finishedCallback = function () {};
-        }
-
-        var osc = ctx.createOscillator();
-
-        osc.type = type;
-
-        osc.connect(ctx.destination);
-        osc.noteOn(0);
-
-        setTimeout(function () {
-            osc.noteOff(0);
-            finishedCallback();
-        }, duration);
-
-    };
-})();
+var sound = new Howl({
+  urls: ['/static/audio/puck.mp3', '/static/audio/puck.ogg', '/static/audio/puck.wav'],
+  autoplay: false,
+  loop: false,
+  volume: 1,
+  onend: function() {
+    console.log('Finished!');
+  }
+});
+var knock = function(){
+    return sound.play();
+}
