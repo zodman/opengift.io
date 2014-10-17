d6f518702db2e48311bd510fcc36d6b69cc04893
Development Server <tonakai.personal@gmail.com>
2014-10-17 11:26:42 +0200

    Added settings for dev host, ignoring gunicorn log and pid files

--- tracker/settings_dev.py
+++ tracker/settings_dev.py
@@ -5,7 +5,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'tracker',                      # Or path to database file if using sqlite3.
-        'USER': 'heliard',                      # Not used with sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
         'PASSWORD': 'godlike999',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
