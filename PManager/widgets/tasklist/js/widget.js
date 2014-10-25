cfa0217cbacce48979c90aabf55b2f14053d4405
Vovaldi <SiNteZ.90@gmail.com>
2014-10-25 15:57:50 +0400

    697 Меню фильтра на js

--- PManager/widgets/tasklist/js/widget.js
+++ PManager/widgets/tasklist/js/widget.js
@@ -1185,3 +1185,38 @@ var widget_tl, currentGroup;
 $.fn.singleActivate = function(){
     return this.addClass('active').siblings().removeClass('active').end();
 };
+
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
+if(isMobile.any()){
+    $('.input-group-btn').addClass('mobile-menu');
+    (function($){
+        $(document).ready(function(){
+            $('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+                event.preventDefault(); 
+                event.stopPropagation(); 
+                $(this).parent().siblings().removeClass('open');
+                $(this).parent().toggleClass('open');
+            });
+        });
+    })(jQuery);
+}
