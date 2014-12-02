d58a0966b271cc950178d848d75580f996f7ad92
gvammer <gvamm3r@gmail.com>
2014-12-02 20:45:29 +0300

    получение пользователей

--- PManager/viewsExt/users.py
+++ PManager/viewsExt/users.py
@@ -18,14 +18,14 @@ class userHandlers:
         resps = widgetManager.getResponsibleList(request.user, None)
         if request.REQUEST.get('q'):
             q = request.REQUEST.get('q')
-            resps = resps.filter(Q(Q(first_name__contains=q) | Q(last_name__contains=q)))
+            resps = resps.filter(Q(Q(firstname__contains=q) | Q(lastname__contains=q)))
 
         aResps = []
         for resp in resps:
             p = resp.get_profile()
             resp = {
-                'first_name': resp.fist_name,
-                'last_name': resp.fist_name,
+                'firstname': resp.fistname,
+                'lastname': resp.fistname,
                 'avatar': p.avatarSrc,
                 'id': resp.id
             }
