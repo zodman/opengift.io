0465f72b89915045b2ceadcdf6c1b1f7b1af78b9
sintez <sintez.90@gmail.com>
2014-12-10 23:28:26 +0300

    766 Слайдер в шапке промо-страницы heliard

--- PManager/static/js/slider/scripts.js
+++ PManager/static/js/slider/scripts.js
@@ -7,6 +7,14 @@ $(document).ready(function(){
 						pause:4000,
 						mode: "fade"
 					  });
+					  $('#head_slider').bxSlider({
+					    pager:false,
+						auto:true,
+						controls:false,
+						pause:6000,
+						speed:1500,
+						mode: "horizontal"
+					  });
 					});
 					
 });
