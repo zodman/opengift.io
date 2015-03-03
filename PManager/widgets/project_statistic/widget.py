430a4171831672a6b94c06fe8c5c76b96717db6f
gvammer <gvamm3r@gmail.com>
2015-03-04 01:45:14 +0300

    правка багов и добавление отчета

--- PManager/widgets/project_statistic/widget.py
+++ PManager/widgets/project_statistic/widget.py
@@ -2,7 +2,7 @@
 __author__ = 'Gvammer'
 from PManager.viewsExt.tasks import TaskWidgetManager
 from PManager.viewsExt.tools import templateTools
-from PManager.models import Payment, Credit, PM_Project
+from PManager.models import Payment, Credit, PM_Project, PM_Timer
 from django.db import connection
 from django.contrib.auth.models import User
 
@@ -40,18 +40,18 @@ class PaymentChart(Chart):
     type = 'chart'
     payQuery = ''
     def getData(self):
-        dayGenerator = (self.dateFrom + datetime.timedelta(x + 1) for x in xrange((self.dateTo - self.dateFrom).days))
+        self.dayGenerator = [self.dateFrom + datetime.timedelta(x + 1) for x in xrange((self.dateTo - self.dateFrom).days)]
         aSums = []
         sOut = 0
         sIn = 0
         pIn = 0
         pOut = 0
-
+        self.xAxe = []
         self.yAxes = {
-            'in': Axis(u'Долговые обязательства', '#0bd145'),
-            'out': Axis(u'Кредиты', '#003060'),
-            'pin': Axis(u'Входящие платежи', 'rgba(63, 255, 0, 0.34)'),
-            'pout': Axis(u'Исходящие платежи', 'rgba(0, 255, 232, 0.34)'),
+            'in': Axis(u'Бонусов списано', '#0bd145'),
+            'out': Axis(u'Бонусов начислено', '#003060'),
+            # 'pin': Axis(u'Входящие платежи', 'rgba(63, 255, 0, 0.34)'),
+            'pout': Axis(u'Погасили бонусов', 'rgba(0, 255, 232, 0.34)'),
         }
 
         def dateToDb(date, type):
@@ -79,7 +79,7 @@ class PaymentChart(Chart):
         for x in cursor.fetchall():
             paymentsIn.append(x)
 
-        for day in dayGenerator:
+        for day in self.dayGenerator:
             credit = Credit.objects.filter(
                 date__range=(datetime.datetime.combine(day, datetime.time.min),
                              datetime.datetime.combine(day, datetime.time.max))
@@ -95,9 +95,7 @@ class PaymentChart(Chart):
 
 
             payments = Payment.objects.filter(date__range=(datetime.datetime.combine(day, datetime.time.min),
-                                                           datetime.datetime.combine(day, datetime.time.max))
-
-                                            )
+                                                           datetime.datetime.combine(day, datetime.time.max)))
             if self.projects:
                 payments = payments.filter(project__in=self.projects)
 
@@ -109,11 +107,11 @@ class PaymentChart(Chart):
             self.xAxe.append(day)
             self.yAxes['in'].values.append(sIn)
             self.yAxes['out'].values.append(sOut)
-            self.yAxes['pin'].values.append(pIn)
+            # self.yAxes['pin'].values.append(pIn)
             self.yAxes['pout'].values.append(pOut)
 
 class sumLoanChart(Chart):
-    title = u'Текущие долги'
+    title = u'Текущие бонусы'
     type = 'table'
     def getData(self):
         arDebts = Credit.getUsersDebt(self.projects)
@@ -130,20 +128,67 @@ class sumLoanChart(Chart):
         for x in arDebts:
             try:
                 user = User.objects.get(pk=int(x['user_id']))
-                self.rows.append({
-                    'cols': [
-                        {
-                            'url': '/user_detail/?id='+str(x[['user_id']]),
-                            'text': user.last_name + ' ' + user.first_name
-                        },
-                        {
-                            'text': x['sum']
-                        }
-                    ]
-                })
+                if x['sum']:
+                    self.rows.append({
+                        'cols': [
+                            {
+                                'url': '/user_detail/?id='+str(x['user_id']),
+                                'text': user.last_name + ' ' + user.first_name
+                            },
+                            {
+                                'text': x['sum']
+                            }
+                        ]
+                    })
             except User.DoesNotExist:
                 pass
 
+class timeChart(Chart):
+    title = u'Потраченное время'
+    type = 'table'
+    def getData(self):
+        from django.db.models import Sum
+        aTimers = PM_Timer.objects.filter(dateEnd__range=(self.dateFrom, self.dateTo))\
+            .values('user') \
+                .annotate(score = Sum('seconds'))
+
+
+        self.cols = [
+            {
+                'name': u'ФИО'
+            },
+            {
+                'name': u'Потрачено времени'
+            },
+            {
+                'name': u'Закрыто по плану'
+            }
+        ]
+        self.rows = []
+        for x in aTimers:
+            try:
+                user = User.objects.get(pk=int(x['user']))
+                closedPlan = user.todo.filter(dateClose__range=(self.dateFrom, self.dateTo))\
+                    .values('planTime').annotate(score=Sum('planTime'))
+                if closedPlan:
+                    closedPlan = closedPlan[0]['score']
+                if x['score']:
+                    self.rows.append({
+                        'cols': [
+                            {
+                                'url': '/user_detail/?id='+str(x['user']),
+                                'text': user.last_name + ' ' + user.first_name
+                            },
+                            {
+                                'text': round(x['score'] / 3600., 2)
+                            },
+                            {
+                                'text': closedPlan or 0
+                            }
+                        ]
+                    })
+            except User.DoesNotExist:
+                pass
 
 def widget(request, headerValues, a, b):
     filt = {}
@@ -174,7 +219,8 @@ def widget(request, headerValues, a, b):
 
     payChart = PaymentChart(filt['dateFrom'], filt['dateTo'], projects)
     loanChart = sumLoanChart(filt['dateFrom'], filt['dateTo'], projects)
-    charts = [payChart, loanChart]
+    tChart = timeChart(filt['dateFrom'], filt['dateTo'], projects)
+    charts = [payChart, loanChart, tChart]
 
 
     return {
