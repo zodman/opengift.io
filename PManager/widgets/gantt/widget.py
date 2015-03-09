cded8a3e791ee46c8a7b35287cfcc21eb0a6e4ad
gvammer <gvamm3r@gmail.com>
2015-03-10 01:43:22 +0300

    тест правки ошибок ганта

--- PManager/widgets/gantt/widget.py
+++ PManager/widgets/gantt/widget.py
@@ -17,6 +17,12 @@ from PManager.viewsExt.tools import templateTools
 def flush_transaction():
     transaction.commit()
 
+def sortGantt(a, b):
+    if not a['virgin'] or not b['virgin']:
+        return 0
+    if a['critically'] == b['critically']:
+        return 0
+    return -1 if a['critically'] > b['critically'] else 1
 
 def widget(request, headerValues, widgetParams={}, qArgs=[]):
     from django.db.models import Q
@@ -150,14 +156,7 @@ def widget(request, headerValues, widgetParams={}, qArgs=[]):
                 task['name'] = ''
                 aTasks.append(task)
 
-            if 'filter' not in widgetParams: # not ajax call
-                def sortGantt(a, b):
-                    if not a['virgin'] or not b['virgin']:
-                        return 0
-                    if a['critically'] == b['critically']:
-                        return 0
-                    return -1 if a['critically'] > b['critically'] else 1
-                aTasks = sorted(aTasks, cmp=sortGantt)
+            aTasks = sorted(aTasks, cmp=sortGantt)
 
     #сначала пробежимся по начатым задачам, чтобы выстроить остальные за ними
     for task in aTasks:
@@ -182,7 +181,7 @@ def widget(request, headerValues, widgetParams={}, qArgs=[]):
     for task in aTasks:
         #если время задачи не задано, его надо расчитать
         if not task['planTime']:
-            task['planTime'] = 2 #TODO: продумать, как можно сделать этот параметр динамическим
+            task['planTime'] = 4 #TODO: продумать, как можно сделать этот параметр динамическим
 
         if task['realDateStart']:
             task['dateCreateGantt'] = task['realDateStart']
