615d1f31a81f439a839829ab20fcb0ee2f782f81
gvammer <gvamm3r@gmail.com>
2015-03-09 23:51:51 +0300

    тест правки ошибок ганта

--- PManager/widgets/gantt/widget.py
+++ PManager/widgets/gantt/widget.py
@@ -150,13 +150,15 @@ def widget(request, headerValues, widgetParams={}, qArgs=[]):
                 task['name'] = ''
                 aTasks.append(task)
 
-    def sortGantt(a, b):
-        if not a['virgin'] or not b['virgin']:
-            return 0
-        if a['critically'] == b['critically']:
-            return 0
-        return -1 if a['critically'] > b['critically'] else 1
-    aTasks = sorted(aTasks, cmp=sortGantt)
+    if 'filter' not in widgetParams: # notajax call
+        def sortGantt(a, b):
+            if not a['virgin'] or not b['virgin']:
+                return 0
+            if a['critically'] == b['critically']:
+                return 0
+            return -1 if a['critically'] > b['critically'] else 1
+        aTasks = sorted(aTasks, cmp=sortGantt)
+
     #сначала пробежимся по начатым задачам, чтобы выстроить остальные за ними
     for task in aTasks:
 
@@ -171,10 +173,11 @@ def widget(request, headerValues, widgetParams={}, qArgs=[]):
                 )
 
                 endTime = task['realDateStart'] + datetime.timedelta(hours=taskTimer.taskRealTime)
-                if endTime < now and not task['closed']: endTime = now
+                if endTime < now and not task['closed']:
+                    endTime = now
+
                 responsibleLastDates = getTaskResponsibleDates(responsibleLastDates, task, endTime)
 
-        # aTasks.append(task)
 
     aTaskMilestones = {}
     for task in aTasks:
