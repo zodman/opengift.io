a382680001389c28fb1e2fd065fa5014388d5b23
gvammer <gvamm3r@gmail.com>
2015-03-10 13:38:55 +0300

    тест блокировки проектов

--- PManager/management/commands/cron_lock_accounts.py
+++ PManager/management/commands/cron_lock_accounts.py
@@ -10,19 +10,17 @@ class Command(NoArgsCommand):
     def handle_noargs(self, **options):
         users = User.objects.filter(
             is_staff=True,
+            is_active=True,
             id__in=PM_User.objects.filter(
                 premium_till__lt=timezone.make_aware(datetime.datetime.now(), timezone.get_default_timezone())
             ).values_list('user__id', flat=True)
         ).exclude(is_superuser=True)
 
-        print PM_User.objects.filter(
-                premium_till__lt=timezone.make_aware(datetime.datetime.now(), timezone.get_default_timezone())
-            ).values_list('user__id', flat=True)
-        print users.values_list('id')
+        aUserIds = users.values_list('id', flat=True)
         users.update(is_staff=False)
-        print users.values_list('username', flat=True)
+        print aUserIds
         print "\r\n"
-        projects = PM_Project.objects.filter(author__in=users, locked=False)
-        projects.update(locked=True)
+        projects = PM_Project.objects.filter(author__in=aUserIds).exclude(locked=True)
         print projects.values_list('name', flat=True)
+        projects.update(locked=True)
         print "\r\n\r\n"
