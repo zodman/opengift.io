824079719ee906e4086df9070d1e24cecb869df3
gvammer <gvamm3r@gmail.com>
2015-03-11 00:43:57 +0300

    убрал пригласить разработчиков heliard и поправил конвертацию мессаг

--- tracker/settings_vagrant.py
+++ tracker/settings_vagrant.py
@@ -3,7 +3,7 @@ __author__ = 'Tonakai'
 DEBUG = True
 # from databases.database import DATABASES
 DATABASES = {
-    'default': {
+    'default_old': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'tracker',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
@@ -19,6 +19,14 @@ DATABASES = {
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
         'TEST_MIRROR': 'default'
+    },
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'tracker_10_03_2015',                      # Or path to database file if using sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': 'root',                  # Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '3306'
     }
 }
 
