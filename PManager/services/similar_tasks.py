98f41a8ad71072d8fb17bfcdf9ba19683fd4c6c7
Artem Smirnov <tonakai.personal@gmail.com>
2015-03-24 11:34:31 +0300

    Changelog: 1.Similar_tasks: Fixed dependencies in similar_tasks service 2.Similar_tasks: Fixed __dict__ issue

--- PManager/services/similar_tasks.py
+++ PManager/services/similar_tasks.py
@@ -1,18 +1,17 @@
 # -*- coding:utf-8 -*-
 __author__ = 'Tonakai'
 from PManager.models.tasks import PM_Task, ObjectTags, PM_Task_Message
-
+from django.db.models import Count
 
 
 def similar_tasks(task_id, limit=4, tagsRelations=[]):
     aSimilarTasks = []
     try:
-        task = PM_Task.objects.get(int(task_id))
+        task = PM_Task.objects.get(pk=int(task_id))
     except (PM_Task.DoesNotExist, ValueError, TypeError):
         return aSimilarTasks
     if not tagsRelations:
         tagsRelations = tags_relations(task)
-    task = task.__dict__
 
     if tagsRelations:
         aTasksFromRelations = []
@@ -30,10 +29,10 @@ def tags_relations(task):
 
 def similar_solutions(task_id, limit=4):
     tasks = similar_tasks(task_id, limit)
+    tasks_ids = set(task.id for task in tasks)
     aMessages = []
     try:
-        messages = PM_Task_Message.objects.filter(task__in=tasks, solution=True)
-        print messages
+        messages = PM_Task_Message.objects.filter(task__in=tasks_ids, solution=True)
         for message in messages:
             aMessages.append(message)
         return aMessages
