d97a1fedaf2e34ed13babe0ddd0cee93125db9f4
Artem Smirnov <tonakai.personal@gmail.com>
2015-04-08 12:17:43 +0300

    stage commit

--- PManager/models/tasklist.py
+++ PManager/models/tasklist.py
@@ -2,28 +2,48 @@
 __author__ = 'Tonakai'
 from django.db import models
 from PManager.models.tasks import PM_Project, PM_Task
-from PManager.models.users import PM_User
+from django.contrib.auth.models import User
 from django.db.models.signals import pre_save
 import datetime
 
+
 class TaskList(models.Model):
+    OPEN = 1
+    CLOSED = 0
     status_choices = (
-        (1, u'Открыт'),
-        (2, u'Закрыт')
+        (OPEN, u'Открыт'),
+        (CLOSED, u'Закрыт')
     )
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
     project = models.ForeignKey(PM_Project, related_name='task_lists')
-    users = models.ManyToManyField(PM_User)
+    users = models.ManyToManyField(User)
+    tasks = models.ManyToManyField(PM_Task)
     closed_at = models.DateTimeField(blank=True, null=True)
-    status = models.IntegerField(blank=True, null=True, default=1, choices=status_choices)
+    status = models.IntegerField(blank=True, null=True, default=CLOSED, choices=status_choices)
+
     def __unicode__(self):
         return "%s#%s" % (self.id, self.project.id)
 
     class Meta:
         app_label = 'PManager'
 
-def closeTaskList(sender, instance, **kwargs):
-    if instance.status == 2:
-        instance.closed_at = datetime.datetime.now()
 
-pre_save.connect(closeTaskList, sender=TaskList)
+def close_task_list(sender, instance, **kwargs):
+    if instance.pk is None:
+        instance.status = TaskList.CLOSED
+        instance.closed_at = None
+        return
+    if instance.status == TaskList.CLOSED \
+            and instance.closed_at is None:
+        old = TaskList.objects.get(pk=instance.id)
+        if old.status != instance.status:
+            instance.closed_at = datetime.datetime.now()
+        return
+    if instance.status == TaskList.OPEN \
+            and (instance.tasks is None
+                 or instance.users is None):
+        instance.status == TaskList.CLOSED
+        return
+
+
+pre_save.connect(close_task_list, sender=TaskList)
--- PManager/tests/models/tasklist_test.py
+++ PManager/tests/models/tasklist_test.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from PManager.models import TaskList
-from PManager.models.tasks import PM_Project, PM_Tracker
+from PManager.models.tasks import PM_Project, PM_Tracker, PM_Task
 from django.contrib.auth.models import User
 from PManager.models.users import PM_User
 import datetime
@@ -8,27 +8,70 @@ import datetime
 class TaskListTestCase(TestCase):
     def setUp(self):
     	self.tracker = PM_Tracker.objects.create(name="Heliant", code="heliant")
-    	self.usr = User.objects.create(email="Test", password="123123")
+        self.usr = User.objects.create(username="test1", email="test@test.com", password="123123")
+        self.usr2 = User.objects.create(username="test2", email="test2@test.com", password="123123")
         self.pro = PM_Project.objects.create(name="test", description="test", locked=0, author=self.usr, tracker=self.tracker)
-        self.tlOpen = TaskList.objects.create(project=self.pro, status=1)
-        self.tlClosed = TaskList.objects.create(project=self.pro, status=2)
+        self.task1 = PM_Task.objects.create(name="TEST TASK 1", project=self.pro, author=self.usr)
+        self.task2 = PM_Task.objects.create(name="TEST TASK 2", project=self.pro, author=self.usr)
 
     def test_tasklist_test(self):
-        tlOpen = TaskList.objects.get(status=1)
-        tlClosed = TaskList.objects.get(status=2)
-        self.assertEqual(tlOpen.status, 1)
-        self.assertEqual(tlClosed.status, 2)
+        tl = TaskList.objects.create(project=self.pro)
+        self.assertIsNotNone(tl.pk, "TaskList should be saved properly")
+        self.assertEqual(tl.status, TaskList.CLOSED, "TaskList should be created with default closed status")
 
-    def test_tasklist_add_date_onclose(self):
-    	tl = TaskList.objects.create(project=self.pro, status=1)
-    	self.assertEqual(tl.status, 1)
-    	tl.status = 2
+    def test_tasklist_closed_at_should_be_null_at_start(self):
+        tl = TaskList.objects.create(project=self.pro)
+        self.assertIsNone(tl.closed_at, "Closed_at field should not be setted immedietly after creation")
+
+    def test_tasklist_forced_close_if_empty_tasks_or_users(self):
+        tl = TaskList.objects.create(project=self.pro, status=TaskList.OPEN)
+        self.assertEqual(tl.status, TaskList.CLOSED, "Status should be closed after list creation")
+        tl.status = TaskList.OPEN
+        tl.save()
+        tl2 = TaskList.objects.get(pk=tl.id)
+        self.assertEqual(tl2.status, TaskList.CLOSED, "TaskList should be saved with forced data on pre_save signal")
+
+    def test_should_not_add_closed_at_at_second_save(self):
+        tl = TaskList.objects.create(project=self.pro)
+        tl.save()
+        self.assertIsNone(tl.closed_at, "Closed_at field should not be setted on re-save")        
+        tl.status = TaskList.CLOSED
+        tl.save()
+        self.assertIsNone(tl.closed_at, "Closed_at field should not be setted on re-save")        
+
+    def test_tasklist_would_open(self):
+        tl = TaskList.objects.create(project=self.pro)
+        tl.users.add(self.usr)
+        tl.tasks.add(self.task1)
+        tl.status = TaskList.OPEN
+        tl.save()
+        self.assertEqual(tl.status, TaskList.OPEN)
+
+    def test_tasklist_closing_should_set_closed_at(self):
+        tl = TaskList.objects.create(project=self.pro)
+        self.assertIsNone(tl.closed_at)
+        tl.users.add(self.usr)
+        tl.tasks.add(self.task1)
+        tl.status = TaskList.OPEN
+        tl.save()
+        self.assertIsNone(tl.closed_at)
+        tl.status = TaskList.CLOSED
+        tl.save()
+        self.assertIsNotNone(tl.closed_at)
+
+    def test_tasklist_add_users_and_tasks(self):
+        #default status should be closed
+    	tl = TaskList.objects.create(project=self.pro)
+    	self.assertEqual(tl.status, TaskList.CLOSED)
+    	tl.tasks.add(self.task1)
+        tl.users.add(self.usr)
+        tl.status = TaskList.OPEN
     	tl.save()
+        #status with tasks and users should be open
+        self.assertEqual(tl.status, TaskList.OPEN)
     	tl2 = TaskList.objects.get(pk=tl.id)
-    	self.assertIsNotNone(tl2.closed_at)
+        self.assertIsNotNone(tl2.users)
+        self.assertIsNotNone(tl2.tasks)
 
-
-def log_(variable):
-	print "######################"
-	print str(variable)
-	print "######################"
+    def test_tasklist_close_on_empty_tasks(self):
+        tl = TaskList.objects.create(project=self.pro)
