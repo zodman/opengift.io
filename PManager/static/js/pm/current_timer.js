6ad890f5f887bfb762ba17f37afd1c4e62871267
gvammer <gvamm3r@gmail.com>
2015-04-20 17:21:51 +0300

    test

--- PManager/static/js/pm/current_timer.js
+++ PManager/static/js/pm/current_timer.js
@@ -124,20 +124,20 @@ $(function(){
                                 }
                             }
                         }
-                        if (CURRENT_TASK_VIEW && CURRENT_TASK_VIEW.model.id == data.id){
-                            for (var k in data){
-                                CURRENT_TASK_VIEW.model.set(k, data[k]);
-                            }
-
-                            if (data.started === true){
-                                t.start();
-                            }else if (data.started === false){
-                                if (CURRENT_TASK_VIEW.model.get('started'))
-                                    t.stop();
-                            }
-
-                            CURRENT_TASK_VIEW.render();
-                        }
+//                        if (CURRENT_TASK_VIEW && CURRENT_TASK_VIEW.model.id == data.id){
+//                            for (var k in data){
+//                                CURRENT_TASK_VIEW.model.set(k, data[k]);
+//                            }
+//
+//                            if (data.started === true){
+//                                t.start();
+//                            }else if (data.started === false){
+//                                if (CURRENT_TASK_VIEW.model.get('started'))
+//                                    t.stop();
+//                            }
+//
+//                            CURRENT_TASK_VIEW.render();
+//                        }
                     }
                 });
 
