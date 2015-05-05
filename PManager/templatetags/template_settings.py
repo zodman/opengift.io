0bde4f5fdf9db8814933e6c1cdb7ffd89627ca7d
Artem Smirnov <tonakai.personal@gmail.com>
2015-05-05 11:35:40 +0300

    Refactored email templates. Replaced hardcode values for host and emails.

---
+++ PManager/templatetags/template_settings.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+__author__ = 'Rayleigh'
+from django.template import Library
+from tracker import settings
+
+register = Library()
+
+
+@register.filter(name='get_settings')
+def get_settings(value):
+    accessible_values = (
+        'GITOLITE_ACCESS_URL',
+        'SOCKET_SERVER_ADDRESS',
+        'STATIC_URL',
+        'SERVER_ROOT_URL',
+        'HTTP_ROOT_URL',
+        'SITE_EMAIL'
+    )
+    if hasattr(settings, value) and value in accessible_values:
+        return settings[value]
+    return ""
