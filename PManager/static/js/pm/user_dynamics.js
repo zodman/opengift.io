61cee32f87211525ec346a070822f815b84649f8
Alwx <alwxsin@gmail.com>
2015-06-17 14:52:35 +0300

    Всплывающие сообщения теперь выдает либа toastr

--- PManager/static/js/pm/user_dynamics.js
+++ PManager/static/js/pm/user_dynamics.js
@@ -5,18 +5,41 @@
  * Time: 18:24
  */
 var userDynamics = {
-    'getOpenTask': function(){
-        if (taskManager){
+    'getOpenTask': function () {
+        if (taskManager) {
             taskManager.taskAjaxRequest({
                 'action': 'getUserLastOpenTask'
-            }, function(task){
+            }, function (task) {
                 if (task && task['name']) {
-                    $.bootstrapGrowl(
-                        "Вы можете вернуться к задаче <a href='" + task['url'] + "'>"
-                            + task['project__name'] + '/ ' + task['name'] + "</a>",
+                    //$.bootstrapGrowl(
+                    //    "Вы можете вернуться к задаче <a href='" + task['url'] + "'>"
+                    //        + task['project__name'] + '/ ' + task['name'] + "</a>",
+                    //    {
+                    //        'delay': 0,
+                    //        'width': 'auto'
+                    //    }
+                    //);
+                    toastr.success(
+                        "<a href='" + task['url'] + "'>" + task['project__name'] + '/ '
+                        + task['name'] + "</a>" + "<br /><br /><button type='button' class='btn clear'>OK</button>",
+                        'Вы можете вернуться к задаче',
                         {
-                            'delay': 0,
-                            'width': 'auto'
+                            "closeButton": true,
+                            "debug": false,
+                            "newestOnTop": false,
+                            "progressBar": false,
+                            "positionClass": "toast-top-full-width",
+                            "preventDuplicates": false,
+                            "onclick": null,
+                            "showDuration": "300",
+                            "hideDuration": "1000",
+                            "timeOut": 0,
+                            "extendedTimeOut": 0,
+                            "showEasing": "swing",
+                            "hideEasing": "linear",
+                            "showMethod": "fadeIn",
+                            "hideMethod": "fadeOut",
+                            "tapToDismiss": false
                         }
                     );
                 }
