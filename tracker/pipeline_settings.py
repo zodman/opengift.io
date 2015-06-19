5ceb20fe94c8802cd8e00ec55544b86a95031fa7
Artem Smirnov <tonakai.personal@gmail.com>
2015-06-19 09:40:47 +0300

    Moved widgets js to static

--- tracker/pipeline_settings.py
+++ tracker/pipeline_settings.py
@@ -63,21 +63,103 @@ PIPELINE_JS = {
         ),
         'output_filename': 'js_compressed/file_up.js'
     },
+    'chat': {
+        'source_filenames': (
+            'js/pm/comments.js',
+            'widgets/chat/widget.js',
+
+        ),
+        'output_filename': 'js_compressed/chat.js'
+    },
+    'file_list': {
+        'source_filenames': (
+            'widgets/file_list/widget.js',
+        ),
+        'output_filename': 'js_compressed/file_list.js'
+    },
     'gantt': {
         'source_filenames': (
-            'gantt/js/dragndrop.js',
-            'gantt/js/gantt.event.block.js',
-            'gantt/js/gantt.js',
-            'gantt/js/gantt.thumbnail.js',
-            'gantt/js/widget.js',
+            'widgets/gantt/js/dragndrop.js',
+            'widgets/gantt/js/gantt.event.block.js',
+            'widgets/gantt/js/gantt.js',
+            'widgets/gantt/js/gantt.thumbnail.js',
+            'widgets/gantt/js/widget.js',
+            'js/libs/fullscreen.js',
         ),
         'output_filename': 'js_compressed/gantt.js'
     },
     'kanban': {
         'source_filenames': (
-            'kanban/js/kanban.js',
+            'js/libs/jquery-ui.custom.min.js',
+            'js/libs/chosen.jquery.min.js'
+            'widgets/kanban/widget.js',
         ),
         'output_filename': 'js_compressed/kanban.js'
+    },
+    'life': {
+        'source_filenames': (
+            'widgets/life/widget.js',
+            'js/libs/charts.js'
+        ),
+        'output_filename': 'js_compressed/life.js'
+    },
+    'profile_edit': {
+        'source_filenames': (
+            'widgets/profile_edit/widget.js',
+        ),
+        'output_filename': 'js_compressed/profile_edit.js'
+    },
+    'project_calendar': {
+        'source_filenames': (
+            'widgets/project_calendar/widget.js',
+        ),
+        'output_filename': 'js_compressed/project_calendar.js'
+    },
+    'project_edit': {
+        'source_filenames': (
+            'js/libs/transliterate.js',
+            'widgets/project_edit/widget.js',
+        ),
+        'output_filename': 'js_compressed/project_edit.js'
+    },
+    'project_graph': {
+        'source_filenames': (
+            'widgets/project_graph/widget.js',
+        ),
+        'output_filename': 'js_compressed/project_graph.js'
+    },
+    'project_statistic': {
+        'source_filenames': (
+            'js/libs/charts.js',
+            'widgets/project_statistic/widget.js',
+        ),
+        'output_filename': 'js_compressed/project_statistic.js'
+    },
+    'project_summary': {
+        'source_filenames': (
+            'widgets/project_summary/widget.js',
+        ),
+        'output_filename': 'js_compressed/project_summary.js'
+    },
+    'task_detail': {
+        'source_filenames': (
+            'js/pm/comments.js',
+            'widgets/task_detail/widget.js',
+        ),
+        'output_filename': 'js_compressed/task_detail.js'
+    },
+    'task_edit': {
+        'source_filenames': (
+            'widgets/task_edit/widget.js',
+        ),
+        'output_filename': 'js_compressed/task_edit.js'
+    },
+    'tasklist': {
+        'source_filenames': (
+            'widgets/tasklist/widget.js',
+            'js/pm/show_tutorial.js',
+        ),
+        'output_filename': 'js_compressed/tasklist.js'
     },
     'user_detail': {
         'source_filenames': (
@@ -88,7 +170,20 @@ PIPELINE_JS = {
             'js/libs/d3.js'
         ),
         'output_filename': 'js_compressed/user_detail.js'
-    }
+    },
+    'user_list': {
+        'source_filenames': (
+            'js/bootstrap/bootstrap-tooltip.js'
+            'widgets/user_list/widget.js',
+        ),
+        'output_filename': 'js_compressed/user_list.js'
+    },
+    'user_statistic': {
+        'source_filenames': (
+            'widgets/user_statistic/widget.js',
+        ),
+        'output_filename': 'js_compressed/user_statistic.js'
+    },
 }
 
 PIPELINE_CSS = {
