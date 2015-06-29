0628b9a51c66449a8a248a25271ae7ce58c33497
gvammer <gvamm3r@gmail.com>
2015-06-30 00:57:43 +0300

    Merge branches 'dev_server' and 'feature/show_messages' of https://bitbucket.org/Gvammer/heliard into dev_server
    # Conflicts:
    #	PManager/static/js/pm/comments.js

--- PManager/static/js/pm/comments.js
+++ PManager/static/js/pm/comments.js
@@ -611,10 +611,10 @@ var SYSTEM_AVATAR_SRC = '/static/images/avatar_red_eye.png';
                 /* Minimize messages */
 
                 var $subContainer = $('.SUBCONTAINER'),
-                    maxVisibleCommentsOnLoadPage = 7, lastItems=4,
-                    firstItems;
+                    firstItems,
+                    lastItems = 6;
 
-                if ($('.SUBCONTAINER:lt(2)').hasClass('MESSAGES')) {
+                if ($('.SUBCONTAINER:lt(2)').hasClass('MESSAGES') || !$subContainer.hasClass('MESSAGES')) {
                     firstItems = 2
                 } else {
                     firstItems = $('.MESSAGES').index('.SUBCONTAINER') + 1
