a448dc94df924533086a7fc0abd8b231743ecf9a
gvammer <gvamm3r@gmail.com>
2015-07-01 02:51:54 +0300

    fnc_ajax fix in comments

--- PManager/classes/git/warden.py
+++ PManager/classes/git/warden.py
@@ -105,7 +105,7 @@ class Warden(object):
                     if s.find('ERROR') > 0 or s.find(':') > 0:
                         errorQty += 1
 
-                d['path'] += u'| Ошибок:'+ str(errorQty)
+                d['path'] += u' | Ошибок:'+ str(errorQty)
 
         except IOError:
             return False
