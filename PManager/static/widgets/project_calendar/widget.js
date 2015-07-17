c8a96c61fe2683bbb4de2fe4db5eb0deced2dbf2
gvammer <gvamm3r@gmail.com>
2015-07-17 14:04:12 +0300

    milestones add

--- PManager/static/widgets/project_calendar/widget.js
+++ PManager/static/widgets/project_calendar/widget.js
@@ -1,28 +1,28 @@
-var ShowForm = function(el, arParams) {
+var ShowForm = function (el, arParams) {
     $(".js-milestone-form").find("input,select").each(
-        function(){
+        function () {
             obj = $(this);
-            if (obj.attr("name") == 'ID'){
+            if (obj.attr("name") == 'ID') {
                 obj.val(arParams['id']);
-            }else if(obj.attr("name") == 'TITLE'){
+            } else if (obj.attr("name") == 'TITLE') {
                 obj.val($(el).parent().find('a').eq(0).text());
-            }else if(obj.attr("name") == 'DEADLINE'){
+            } else if (obj.attr("name") == 'DEADLINE') {
                 obj.val(arParams['deadline']);
-            }else if(obj.attr("name") == 'RESPONSIBLE_ID' && arParams['resp_id']){
+            } else if (obj.attr("name") == 'RESPONSIBLE_ID' && arParams['resp_id']) {
                 $(this).val(arParams['resp_id']);
-            }else if(obj.attr("name") == 'PRIORITY'){
+            } else if (obj.attr("name") == 'PRIORITY') {
                 $(this).val(arParams['prior']);
-            }else if(obj.attr("name") == 'GROUP_ID' && arParams['project_id']){
+            } else if (obj.attr("name") == 'GROUP_ID' && arParams['project_id']) {
                 $(this).val(arParams['project_id']);
             }
         }
     );
 
     var group_nm = $(el).parents('tr').eq(0).find('.grp_name').eq(0).text();
-    if (arParams['project_id']){
+    if (arParams['project_id']) {
         $('#title_inp').val('');
         $('#header_form').text(group_nm);
-    }else{
+    } else {
         $('#header_form').text(group_nm);
     }
 
@@ -32,45 +32,47 @@ var ShowForm = function(el, arParams) {
     return false;
 };
 
-var ajaxSaveMileStone = function(data){
-    PM_AjaxPost('/milestone_ajax/',data,function(data){
-        $("#taskform").hide();
+var ajaxSaveMileStone = function (data) {
+    PM_AjaxPost('/milestone_ajax/', data, function (data) {
+        $(".js-form-modal").hide();
         document.location.reload();
-    })
+    });
+
+    return false;
 };
 
-var deleteTask = function(id){
-    PM_AjaxPost('/milestone_ajax/',{
-        'action':'remove',
-        'id':id
-    },function(data){
-        $(".js-milestone-"+id).remove();
+var deleteTask = function (id) {
+    PM_AjaxPost('/milestone_ajax/', {
+        'action': 'remove',
+        'id': id
+    }, function (data) {
+        $(".js-milestone-" + id).remove();
     });
     return false;
 };
 
-$(function(){
-    $('.js-milestone-submit').click(function(){
+$(function () {
+    $('.js-milestone-submit').click(function () {
         var $form = $(this).closest('form');
         var data = {
-            'name':$form.find('[name=TITLE]').val(),
-            'responsible':$form.find('[name=RESPONSIBLE_ID]').val(),
-            'date':$form.find('[name=DEADLINE]').val(),
-            'id':$form.find('[name=ID]').val(),
-            'project':$form.find('[name=GROUP_ID]').val()
+            'name': $form.find('[name=TITLE]').val(),
+            'responsible': $form.find('[name=RESPONSIBLE_ID]').val(),
+            'date': $form.find('[name=DEADLINE]').val(),
+            'id': $form.find('[name=ID]').val(),
+            'project': $form.find('[name=GROUP_ID]').val()
         };
 
-        ajaxSaveMileStone(data);
+        return ajaxSaveMileStone(data);
     });
 
-    $("#taskform").click(function(e){
+    $("#taskform").click(function (e) {
         e.stopPropagation();
     });
     $("input[name='DEADLINE']").datetimepicker({
         'format': 'd.m.Y',
         'timepicker': false,
         'closeOnDateSelect': true,
-        'onDateSelect': function(ct){
+        'onDateSelect': function (ct) {
             $(this).val = moment(ct).format('DD.MM.YYYY')
         }
     });
@@ -81,14 +83,14 @@ $(function(){
         minSlides: 2,
         maxSlides: 7,
         pager: false,
-        nextText:'<i class="fa fa-angle-right"></i>',
-        prevText:'<i class="fa fa-angle-left"></i>',
+        nextText: '<i class="fa fa-angle-right"></i>',
+        prevText: '<i class="fa fa-angle-left"></i>',
         hideControlOnEnd: true,
-        onSlideBefore: function($slideElement, oldIndex, newIndex){
+        onSlideBefore: function ($slideElement, oldIndex, newIndex) {
             slideTopProceed = true;
             $sliderBot.goToSlide(newIndex);
         },
-        onSlideAfter: function(){
+        onSlideAfter: function () {
             slideTopProceed = false;
         }
     });
@@ -100,7 +102,7 @@ $(function(){
         pager: false,
         controls: false,
         hideControlOnEnd: true,
-        onSlideBefore: function($slideElement, oldIndex, newIndex){
+        onSlideBefore: function ($slideElement, oldIndex, newIndex) {
             if (!slideTopProceed) {
                 $sliderTop.goToSlide(newIndex);
                 return false;
@@ -109,7 +111,7 @@ $(function(){
     });
 
     var navHeight = $('.calendar-project-list-title-wrapper').offset().top - 59;
-    $(window).bind('scroll', function() {
+    $(window).bind('scroll', function () {
         if ($(window).scrollTop() > navHeight) {
             $('.calendar-project-list-title-wrapper').addClass('fixed');
             $('.calendar-project-list-dates').addClass('top-30');
