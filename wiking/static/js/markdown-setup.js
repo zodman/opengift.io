82ef7ecee755a10aa03e127049fc452fb93c1599
Artem Smirnov <tonakai.personal@gmail.com>
2015-07-23 13:45:42 +0300

    added post-processing for wiking article

--- wiking/static/js/markdown-setup.js
+++ wiking/static/js/markdown-setup.js
@@ -9,6 +9,9 @@
             markdown.find('code').wrap('<pre>');
             // this is a hack - to html method to return actual string,
             // if not this - will be returned first element html only.
+            markdown.find('img').addClass('wiking-content__image');
+            markdown.find('ul').addClass('wiking-content__list');
+            markdown.find('ol').addClass('wiking-content__list');
             return $('<div>').append(markdown).html();
         }
     });
