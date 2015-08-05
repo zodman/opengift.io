d7303a27ecf98e1ed132ae580780f8b4005ac837
gvammer <gvamm3r@gmail.com>
2015-08-05 14:17:55 +0300

    reformat code

--- PManager/models/notice.py
+++ PManager/models/notice.py
@@ -5,6 +5,7 @@ import datetime
 from django.utils import timezone
 from django.db.models import Q
 
+
 class PM_Notice(models.Model):
     name = models.CharField(max_length=100)
     html = models.CharField(max_length=1000)
@@ -17,35 +18,38 @@ class PM_Notice(models.Model):
 
     @property
     def src(self):
-        return str(self.image).replace('PManager/static/upload','')
+        return str(self.image).replace('PManager/static/upload', '')
 
     @staticmethod
-    def getForUser(user,path):
+    def getForUser(user, path):
         if not path: path = '/'
         if user and user.is_authenticated():
-            dateLast = timezone.make_aware(datetime.datetime.now() - datetime.timedelta(minutes=1), timezone.get_default_timezone())
+            dateLast = timezone.make_aware(datetime.datetime.now() - datetime.timedelta(minutes=1),
+                                           timezone.get_default_timezone())
             lastNotice = PM_NoticedUsers.objects.filter(user=user, date__gt=dateLast)
             if not lastNotice:
-                notices = PM_Notice.objects.exclude(pk__in=PM_NoticedUsers.objects.filter(user=user).values('notice__id'))\
-                            .filter(Q(url=path) | Q(url__isnull=True)).order_by('?')
+                notices = PM_Notice.objects.exclude(
+                    pk__in=PM_NoticedUsers.objects.filter(user=user).values('notice__id')) \
+                    .filter(Q(url=path) | Q(url__isnull=True)).order_by('?')
 
                 if notices:
                     return notices[0]
         return None
 
-    def setRead(self,user):
-        return PM_NoticedUsers.objects.create(user=user,notice=self)
+    def setRead(self, user):
+        return PM_NoticedUsers.objects.create(user=user, notice=self)
 
     class Meta:
         app_label = 'PManager'
 
+
 class PM_NoticedUsers(models.Model):
-    user = models.ForeignKey(User,related_name='notices')
-    notice = models.ForeignKey(PM_Notice,related_name='userNotices')
+    user = models.ForeignKey(User, related_name='notices')
+    notice = models.ForeignKey(PM_Notice, related_name='userNotices')
     date = models.DateTimeField(default=datetime.datetime.now().replace(microsecond=0))
 
     def __unicode__(self):
-        return self.notice.name+" - "+self.user.username
+        return self.notice.name + " - " + self.user.username
 
     class Meta:
         app_label = 'PManager'
