8c1cc29a6d77aaf1a6efbadba201b2a1e1d42255
gvammer <gvamm3r@gmail.com>
2015-08-15 02:37:07 +0300

    credit refactor

--- PManager/models/payments.py
+++ PManager/models/payments.py
@@ -6,6 +6,17 @@ from django.db.models.signals import post_save, pre_delete
 from django.db import connection
 
 
+class Payment(models.Model):
+    user = models.ForeignKey(User, related_name='payments', null=True, blank=True)
+    payer = models.ForeignKey(User, related_name='repayments', null=True, blank=True)
+    project = models.ForeignKey(PM_Project, related_name='payments', null=True, blank=True)
+    value = models.IntegerField()
+    date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+
+    class Meta:
+        app_label = 'PManager'
+
+
 class Credit(models.Model):
     user = models.ForeignKey(User, related_name='arrears', null=True, blank=True)
     payer = models.ForeignKey(User, related_name='credits', null=True, blank=True)
@@ -46,3 +57,49 @@ class Credit(models.Model):
 
     class Meta:
         app_label = 'PManager'
+
+
+# SIGNALS
+
+def payment_account(sender, instance, created, **kwargs):
+    if created:
+        if instance.user:
+            profile = instance.user.get_profile()
+            if not profile.account_total: profile.account_total = 0
+            profile.account_total -= instance.value
+            profile.save()
+        if instance.payer:
+            profile = instance.payer.get_profile()
+            if not profile.account_total: profile.account_total = 0
+            profile.account_total += instance.value
+            profile.save()
+
+
+def credit_del_account(sender, instance, using, **kwargs):
+    payment_account(sender, instance, True)
+
+
+post_save.connect(payment_account, sender=Payment)
+pre_delete.connect(credit_del_account, sender=Credit)
+
+
+def credit_account(sender, instance, created, **kwargs):
+    if created:
+        if instance.user:
+            profile = instance.user.get_profile()
+            if not profile.account_total: profile.account_total = 0
+            profile.account_total += instance.value
+            profile.save()
+        if instance.payer:
+            profile = instance.payer.get_profile()
+            if not profile.account_total: profile.account_total = 0
+            profile.account_total -= instance.value
+            profile.save()
+
+
+def payment_del_account(sender, instance, using, **kwargs):
+    credit_account(sender, instance, True)
+
+
+post_save.connect(credit_account, sender=Credit)
+pre_delete.connect(payment_del_account, sender=Payment)
