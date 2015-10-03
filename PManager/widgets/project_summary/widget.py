45dd66db539855f0a930431a829cc5ab7b015ff3
gvammer <gvamm3r@gmail.com>
2015-10-03 21:17:08 +0300

    new project summary

--- PManager/widgets/project_summary/widget.py
+++ PManager/widgets/project_summary/widget.py
@@ -2,7 +2,7 @@
 __author__ = 'Gvammer'
 from PManager.models import PM_Timer, PM_Task, Credit
 import datetime
-
+from django.db.models import Sum
 
 def widget(request, headerValues, a, b):
     projects = request.user.get_profile().managedProjects
@@ -23,26 +23,15 @@ def widget(request, headerValues, a, b):
             ):
             projectSumPayed += o.summ if o.summ else 0
 
-        projectDebt = 0
-        for o in Credit.objects.raw(
-                'SELECT sum(value) as summ, id, project_id from PManager_credit' +
-                ' WHERE `payer_id` IS NOT NULL AND value > 0'
-                ' AND `project_id`=' + str(int(project.id))
-            ):
-            projectDebt += o.summ if o.summ else 0
-
-        projectDebtPayed = 0
-        for o in Credit.objects.raw(
-                'SELECT sum(value) as summ, id, project_id from PManager_credit' +
-                ' WHERE `user_id` IS NOT NULL AND value < 0'
-                ' AND `project_id`=' + str(int(project.id))
-            ):
-            projectDebtPayed += o.summ if o.summ else 0
+        tasks = PM_Task.objects.filter(project=project, closed=False).aggregate(Sum('planTime'))
+        realtime = PM_Timer.objects.filter(task__project=project, task__closed=False).aggregate(Sum('seconds'))
+        realtime = realtime['seconds__sum'] or 0
+        plantime = tasks['planTime__sum'] or 0
 
         setattr(project, 'projectSum', projectSum)
         setattr(project, 'projectSumPayed', projectSumPayed)
-        setattr(project, 'projectDebt', projectDebt)
-        setattr(project, 'projectDebtPayed', projectDebtPayed)
+        setattr(project, 'realtime', round(realtime/3600.0, 2))
+        setattr(project, 'plantime', plantime)
 
     return {
         'projects':projects,
