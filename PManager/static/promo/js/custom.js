f84e23ae3e3aa61a43e8f826c4609c5979bbd7ff
eugene_va <ugeeeen@gmail.com>
2015-11-02 17:18:49 +0300

    Добавить внизу на промо иконки того, что мы делаем - CSS, JavaScript, HTML, PHP, BITRIX, Веб-Дизайн, Логотипы

--- PManager/static/promo/js/custom.js
+++ PManager/static/promo/js/custom.js
@@ -125,7 +125,7 @@ $(document).ready(function () {
     var owl = $("#screenshots");
 
     owl.owlCarousel({
-        items: 2, //10 items above 1000px browser width
+        items: 1, //10 items above 1000px browser width
         itemsDesktop: [1000, 2], //5 items between 1000px and 901px
         itemsDesktopSmall: [900, 2], // betweem 900px and 601px
         itemsTablet: [600, 1], //2 items between 600 and 0
