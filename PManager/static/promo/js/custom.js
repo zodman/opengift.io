a5c1c5c283333033dd3c34ea9c4b26a705a581b7
eugene_va <ugeeeen@gmail.com>
2015-11-03 13:48:53 +0300

    Добавить внизу на промо иконки того, что мы делаем - CSS, JavaScript, HTML, PHP, BITRIX, Веб-Дизайн, Логотипы

--- PManager/static/promo/js/custom.js
+++ PManager/static/promo/js/custom.js
@@ -120,17 +120,25 @@ $(document).ready(function () {
         paginationSpeed: 800,
         autoPlay: 10000,
         singleItem: true
+
     });
 
     var owl = $("#screenshots");
 
     owl.owlCarousel({
         items: 1, //10 items above 1000px browser width
-        itemsDesktop: [1000, 2], //5 items between 1000px and 901px
-        itemsDesktopSmall: [900, 2], // betweem 900px and 601px
+        itemsDesktop: [1000, 1], //5 items between 1000px and 901px
+        itemsDesktopSmall: [900, 1], // betweem 900px and 601px
         itemsTablet: [600, 1], //2 items between 600 and 0
         itemsMobile: false // itemsMobile disabled - inherit from itemsTablet option
     });
+
+    $(".next").click(function(){
+        owl.trigger('owl.next');
+    })
+    $(".prev").click(function(){
+        owl.trigger('owl.prev');
+    })
 
 
 });
