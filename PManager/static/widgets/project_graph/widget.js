3c4b85706da0df686cfb5641842bda849003c869
gvammer <gvamm3r@gmail.com>
2015-11-18 01:42:04 +0300

    правильное кол-во открытых задач в списке пользователей

--- PManager/static/widgets/project_graph/widget.js
+++ PManager/static/widgets/project_graph/widget.js
@@ -11,14 +11,14 @@
         $('[data-toggle="popover"]').popover({
             trigger:'hover'
         });
-    });
-    $('.js-select-role').click(function () {
+        $('.js-select-role').click(function () {
             if (!$(this).is('.active')) {
                 $(this).addClass('active').siblings('.js-select-role').removeClass('active');
                 $('.js-role').filter('[data-role=' + $(this).data('role') + ']').show().siblings('.js-role').hide();
             }
             $('.js-graph-'+$(this).data('role')).show().siblings().hide();
         });
+
         $('.js-change-role').click(function () {
             var $next = $('.js-select-role.active').next('.js-select-role');
             var $first = $('.js-select-role').not('.active').eq(0);
@@ -29,7 +29,9 @@
             }
             return false;
         });
+
         $('.js-graph-'+$('.js-select-role.active').data('role')).show().siblings().hide();
+
         $(window).on('task_closed', function(taskInfo) {
             var $tClosed = $('.js-project-graph-tasks-closed'),
                 $tAll = $('.js-project-graph-tasks-all'),
@@ -38,4 +40,5 @@
             $tClosed.text(parseInt($tClosed.text()) + 1);
             $progress.css('width', Math.round(parseInt($tClosed.text()) * 100 / parseInt($tAll.text())) + '%');
         });
+    });
 })(jQuery);
