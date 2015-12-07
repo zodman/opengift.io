640179f158fe5391805f9998794e237514ec3a46
gvammer <gvamm3r@gmail.com>
2015-12-08 01:52:32 +0300

    статистика

--- PManager/widgets/project_summary/widget.py
+++ PManager/widgets/project_summary/widget.py
@@ -1,11 +1,20 @@
 # -*- coding:utf-8 -*-
 __author__ = 'Gvammer'
-from PManager.models import PM_Timer, PM_Task, Credit
+from PManager.models import PM_Timer, PM_Task, Credit, Fee, PM_Project
 import datetime
 from django.db.models import Sum
 
 def widget(request, headerValues, a, b):
-    projects = request.user.get_profile().getProjects(only_managed=True)
+    if request.user.is_superuser:
+        projects = PM_Project.objects.all()
+    else:
+        projects = request.user.get_profile().getProjects(only_managed=True)
+
+    feeAll = Fee.objects.raw('SELECT SUM(value) s, id from pmanager_fee where value > 0')
+    feeAll = feeAll[0].s
+    feePayed = Fee.objects.raw('SELECT SUM(value) s, id from pmanager_fee where value < 0')
+    feePayed = -feePayed[0].s
+    aProjects = []
     for project in projects:
         projectSum = 0
         for o in Credit.objects.raw(
@@ -14,6 +23,9 @@ def widget(request, headerValues, a, b):
                 ' AND `project_id`=' + str(int(project.id))
             ):
             projectSum += o.summ if o.summ else 0
+
+        if not projectSum:
+            continue
 
         projectSumPayed = 0
         for o in Credit.objects.raw(
@@ -32,8 +44,12 @@ def widget(request, headerValues, a, b):
         setattr(project, 'projectSumPayed', projectSumPayed)
         setattr(project, 'realtime', round(realtime/3600.0, 2))
         setattr(project, 'plantime', plantime)
+        aProjects.append(project)
 
     return {
-        'projects':projects,
-        'title': u'Сводка по проектам'
+        'projects': aProjects,
+        'title': u'Сводка по проектам',
+        'feeAll': feeAll,
+        'feePayed': feePayed,
+        'comission': PM_Task.FEE
     }
