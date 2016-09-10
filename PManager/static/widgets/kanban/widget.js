9ba93ef8f6cf8aa7b955480bd9ece4089a2e5888
gvammer <gvamm3r@gmail.com>
2016-09-11 02:01:00 +0300

    v2.0

--- PManager/static/widgets/kanban/widget.js
+++ PManager/static/widgets/kanban/widget.js
@@ -16,20 +16,20 @@ $(function () {
         EASING_EFFECT = 'easeOutCubic',
         PROJECT_ROW_SELECTOR = '.js-project-row',
         PLACEHOLDER_CLASS = 'task-kanban-dummy',
-        insertTask = function insertTask(index, column, what){
-            if(index === -1){
+        insertTask = function insertTask(index, column, what) {
+            if (index === -1) {
                 return what.appendTo(column);
             }
-            if(index >= 1){
+            if (index >= 1) {
                 return what.insertAfter(column.find(TASK_SELECTOR + ':eq(' + (index - 1) + ')'));
-            }else{
+            } else {
                 return what.prependTo(column);
             }
         },
         animateAppendTo = function animateAppendTo(item, sel, speed, fn) {
             item = $(item);
 
-            var frame = { top: 0, left: 0},
+            var frame = {top: 0, left: 0},
                 origin = item.offset(),
                 whiteSpace = $('<div>').addClass(EMPTY_TASK_CLASS),
                 whiteSpaceOrigin = whiteSpace.clone(),
@@ -37,7 +37,7 @@ $(function () {
                 column = item.parent(),
                 destination, adjustDuration, previndex;
             previndex = parseInt(item.attr(PREVINDEX_NAME));
-            if(isNaN(previndex)){
+            if (isNaN(previndex)) {
                 previndex = -1;
             }
             item.removeAttr(PREVINDEX_NAME);
@@ -55,23 +55,23 @@ $(function () {
             insertTask(currentIndex, column, whiteSpaceOrigin);
             whiteSpaceOrigin.hide({
                 duration: speed,
-                complete: function(){
+                complete: function () {
                     whiteSpaceOrigin.remove();
                 },
                 easing: EASING_EFFECT_WHITESPACE
             });
             adjustDuration = Math.sqrt(
-                Math.pow(destination.top - origin.top, 2) +
-                Math.pow(destination.left - origin.left, 2)
-            ) * speed / 400;
+                    Math.pow(destination.top - origin.top, 2) +
+                    Math.pow(destination.left - origin.left, 2)
+                ) * speed / 400;
             item.animate({
                 top: destination.top - frame.top,
                 left: destination.left - frame.left
-            }, adjustDuration, EASING_EFFECT, function(){
+            }, adjustDuration, EASING_EFFECT, function () {
                 insertTask(previndex, sel, item);
                 whiteSpace.remove();
-                item.attr('style','').removeClass(UI_SORTABLE_HELPER_CLASS);
-                if(typeof(fn) === 'function'){
+                item.attr('style', '').removeClass(UI_SORTABLE_HELPER_CLASS);
+                if (typeof(fn) === 'function') {
                     fn.call(item);
                 }
             });
@@ -89,8 +89,8 @@ $(function () {
                 linkWidth = taskExecutor.innerWidth(),
                 ulOffset = userList.offset(),
                 arrowPos;
-            arrowPos = linkWidth/2 - ARROW_DISTANCE_TO_CENTER;
-            if(ulOffset.left < linkLeftOffset.left){
+            arrowPos = linkWidth / 2 - ARROW_DISTANCE_TO_CENTER;
+            if (ulOffset.left < linkLeftOffset.left) {
                 arrowPos += linkLeftOffset.left - ulOffset.left + ARROW_DISTANCE_TO_CENTER;
             }
             userList.find(ARROW_SELECTOR).css('left', arrowPos);
@@ -105,7 +105,7 @@ $(function () {
                 totalW = $(window).width(),
                 left;
             left = offset.left + 20;
-            if(left + ulistW >= totalW -4){
+            if (left + ulistW >= totalW - 4) {
                 left = totalW - ulistW - 5;
             }
             userList.css({
@@ -137,8 +137,9 @@ $(function () {
             this.initColumns();
             this.initRequest();
             this.getTasksFromServer();
+            this.initListeners();
         },
-        initColumns: function initColumns(){
+        initColumns: function initColumns() {
             var column;
             this.$columns = this.element.find(TASK_COLUMN_SELECTOR);
             for (var i = this.$columns.length - 1; i >= 0; i--) {
@@ -146,15 +147,15 @@ $(function () {
                 this._columns[$(column).attr(REL_ATTRIBUTE)] = $(column);
             }
         },
-        initOptions: function initOptions(){
+        initOptions: function initOptions() {
             this.options.project = this.element.data(PROJECT_DATA_NAME);
             this.options.milestone_id = this.element.data(PROJECT_MILESTONE_ID);
             this.options.useColors = this.element.data('use_colors');
-            if(this.options.useColors){
+            if (this.options.useColors) {
                 this.columnProperty = 'color';
             }
         },
-        initRequest: function initRequest(){
+        initRequest: function initRequest() {
             var t = this,
                 propName = this.options.useColors == false ? 'status__code' : this.columnProperty,
                 props = [propName],
@@ -164,6 +165,19 @@ $(function () {
             }
             this.ajax.props = props;
             this.ajax.propVals = propVals;
+        },
+        initListeners: function () {
+            baseConnector.addListener('fs.comment.add', function (message) {
+                if (message.project.id != window.currentProject) return;
+                var mesType, code = (message.code + '').toLowerCase();
+                if (code == 'warning') {
+                    mesType = "warning"
+                }
+                else {
+                    mesType = "info"
+                }
+                toastr[mesType]((message.task ? '<a href="' + message.task.url + '">Новое сообщение</a><br />' : '') + message.text + '</a>');
+            });
         },
         getTasksFromServer: function getTasksFromServer() {
             var t = this;
@@ -196,7 +210,7 @@ $(function () {
                 'json'
             )
         },
-        getXls: function() {
+        getXls: function () {
             var t = this;
 
             PM_AjaxPost(
@@ -239,7 +253,7 @@ $(function () {
                 }
                 var view = t.taskViews[data.id],
                     oldColumn = view.$el.parent().attr(REL_ATTRIBUTE),
-                    returnToFix = function(){
+                    returnToFix = function () {
                         return (data['status'] == REVISION_COLUMN_CODE &&
                             ($.inArray(view.model.get('status'), [READY_COLUMN_CODE, TODAY_COLUMN_CODE]) > -1) &&
                             view.model.get('resp') && view.model.get('resp')[0] &&
@@ -256,7 +270,7 @@ $(function () {
                             view.model.set('viewed', false);
                         }
                     }
-                    if(i == 'resp' && data[i][0] && data[i][0].hasOwnProperty('avatar')){
+                    if (i == 'resp' && data[i][0] && data[i][0].hasOwnProperty('avatar')) {
                         view.model.set('avatar', data[i][0].avatar);
                     }
                     if (i != 'id') {
@@ -267,7 +281,7 @@ $(function () {
                 view.render();
                 var col = view.model.get(t.columnProperty);
                 if (col && oldColumn != col) {
-                    animateAppendTo(view.$el, t._columns[col], t.options.animateDuration, function(){
+                    animateAppendTo(view.$el, t._columns[col], t.options.animateDuration, function () {
                         t.addTaskToColumn(view.model.id, col, oldColumn);
                     });
                 }
@@ -298,7 +312,7 @@ $(function () {
                 dropOnEmpty: true,
                 revert: t.options.revertDuration,
                 appendTo: $(t.options.appendItemsTo),
-                start: function(event, ui) {
+                start: function (event, ui) {
                     var taskid = ui.item.data('taskid'),
                         view = t.taskViews[taskid];
                     ui.item.attr(PREVINDEX_NAME, ui.item.index());
@@ -306,33 +320,33 @@ $(function () {
                     view.model.set('oldStatusName', t.columnProperty);
                     view.model.set('oldStatus', view.model.get(t.columnProperty));
                 },
-                receive: function(event, ui){
+                receive: function (event, ui) {
                     var toColumn = $(event.target).attr(REL_ATTRIBUTE);
                     var fromColumn = ui.sender.attr(REL_ATTRIBUTE);
                     var taskid = ui.item.data(t.options.taskidKey);
                     t.addTaskToColumn(taskid, toColumn, fromColumn);
-                    if(toColumn == TODAY_COLUMN_CODE){
+                    if (toColumn == TODAY_COLUMN_CODE) {
                         t.taskViews[taskid].model.set(t.columnProperty, toColumn);
                         t.taskViews[taskid].render();
                         return;
                     }
-                    var cb = function(data){
-                                try{
-                                    data = $.parseJSON(data);
-                                } catch(e){
-                                    console.log(e);
-                                    return;
-                                }
-                                if(data.error){
-                                    alert(data.error);
-                                    animateAppendTo(t.taskViews[taskid].$el, t._columns[fromColumn], t.options.animateDuration, function(){
-                                        t.$columns.sortable('refreshPositions');
-                                    });
-                                }else{
-                                    t.taskViews[taskid].model.set(t.columnProperty, toColumn);
-                                    t.taskViews[taskid].render();
-                                }
-                            };
+                    var cb = function (data) {
+                        try {
+                            data = $.parseJSON(data);
+                        } catch (e) {
+                            console.log(e);
+                            return;
+                        }
+                        if (data.error) {
+                            alert(data.error);
+                            animateAppendTo(t.taskViews[taskid].$el, t._columns[fromColumn], t.options.animateDuration, function () {
+                                t.$columns.sortable('refreshPositions');
+                            });
+                        } else {
+                            t.taskViews[taskid].model.set(t.columnProperty, toColumn);
+                            t.taskViews[taskid].render();
+                        }
+                    };
 
                     if (toColumn == 'closed') {
                         taskManager.TaskClose(taskid, cb);
@@ -355,12 +369,12 @@ $(function () {
             }
             var task = new window.taskClass(taskData);
             var taskView = new window.taskViewKanbanClass({'model': task});
-            if(t._columns[key]){
+            if (t._columns[key]) {
                 task.set(t.columnProperty, key);
                 taskView.createEl().render();
                 t._columns[key].append(taskView.$el);
                 t.taskViews[task.id] = taskView;
-            }else {
+            } else {
                 console.log('error encountered, task could not resolve column');
                 return false;
             }
@@ -371,13 +385,13 @@ $(function () {
         var t = this;
         $(this).kanban();
 
-        $(this).find('.js-get-xls-milestone').click(function() {
+        $(this).find('.js-get-xls-milestone').click(function () {
             $(t).kanban('getXls');
             return false;
         });
     });
 
-    $('.js-select-milestone').change(function() {
+    $('.js-select-milestone').change(function () {
         document.location.href = '?milestone=' + $(this).val();
     });
 });
