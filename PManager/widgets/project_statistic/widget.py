b09aef553f8fc5a38d381e1699d7c89ec8bbeec1
emaslov <e.maslov@mangotele.com>
2017-11-30 18:53:25 +0300

    New public page

--- PManager/widgets/project_statistic/widget.py
+++ PManager/widgets/project_statistic/widget.py
@@ -181,9 +181,9 @@ class ViewDownloadChart(Chart):
         self.xAxe = []
         r = lambda: random.randint(0, 255)
         self.yAxes = {
-            u'Просмотры': Axis(u'Просмотры', 'rbga(138, 85, 96, 1)'),
-            u'Скачивания': Axis(u'Скачивания', 'rbga(236, 97, 183, 1)'),
-            u'Подписчики': Axis(u'Подписчики', 'rbga(56, 195, 209, 1)')
+            u'Просмотры': Axis(u'Просмотры', 'rgba(138,85,96,1)'),
+            u'Скачивания': Axis(u'Скачивания', 'rgba(236,97,183,1)'),
+            u'Подписчики': Axis(u'Подписчики', 'rgba(56,195,209,1)')
         }
 
         for day in self.dayGenerator:
@@ -207,7 +207,7 @@ class BugsChart(Chart):
         self.xAxe = []
         r = lambda: random.randint(0, 255)
         self.yAxes = {
-            u'Ошибки': Axis(u'Ошибки', 'rbga(99, 137, 228, 1)')
+            u'Ошибки': Axis(u'Ошибки', 'rgba(99,137,228,1)')
         }
 
         for day in self.dayGenerator:
@@ -235,7 +235,7 @@ class DonationsChart(Chart):
         self.xAxe = []
         r = lambda: random.randint(0, 255)
         self.yAxes = {
-            u'Донейты': Axis(u'Донейты', 'rbga(94, 186, 150, 1)')
+            u'Донейты': Axis(u'Донейты', 'rgba(94,186,150,1)')
         }
 
         for day in self.dayGenerator:
