122d077d66c8b1eb55172e97ad62283074ccd415
emaslov <e.maslov@mangotele.com>
2017-11-30 14:25:38 +0300

    New public page

--- PManager/widgets/project_statistic/widget.py
+++ PManager/widgets/project_statistic/widget.py
@@ -524,7 +524,7 @@ def widget(request, headerValues, a, b):
 
     charts = []
     if 'getAllCharts' in headerValues:
-        now = now - datetime.timedelta(days=665)
+        now = now - datetime.timedelta(days=365)
         filt['dateFrom'] = now - datetime.timedelta(days=365)
         filt['dateTo'] = now
         for chartName in ['TaskCommitsChart', 'ViewDownloadChart', 'BugsChart', 'DonationsChart']:
