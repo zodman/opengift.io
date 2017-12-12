1d4b4208486f77febbba1697e198cc9ab19ef436
gvammer <gvamm3r@gmail.com>
2017-12-13 01:09:57 +0300

    rating and likes

--- PManager/models/tasks.py
+++ PManager/models/tasks.py
@@ -99,6 +99,7 @@ class PM_Tracker(models.Model):
 
 class PM_Project_Industry(models.Model):
     name = models.CharField(max_length=255, verbose_name=u'Название')
+    parent_id = models.ForeignKey('PM_Project_Industry', related_name="problemss", null=True, blank=True)
     parent = models.ForeignKey('PM_Project_Industry', related_name="problems", null=True, blank=True)
 
     class Meta:
