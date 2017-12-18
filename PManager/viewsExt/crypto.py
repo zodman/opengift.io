d8e4b595c1d0dc5a360bb958d3b7c009a9e5aa06
gvammer <gvamm3r@gmail.com>
2017-12-19 00:38:57 +0300

    rating and likes

--- PManager/viewsExt/crypto.py
+++ PManager/viewsExt/crypto.py
@@ -5,10 +5,7 @@ CRYPTO_HOST = '188.166.237.19'
 def bitcoin_set_request(project_id, sum):
     service_url = '/bitcoin/request/create'
 
-    fp = urllib.request.urlopen("http://" + CRYPTO_HOST + service_url + '?project='+str(project_id)+'&sum='+str(sum))
-    mybytes = fp.read()
-
-    res = mybytes.decode("utf8")
-    fp.close()
+    fp = urllib.urlopen("http://" + CRYPTO_HOST + service_url + '?project='+str(project_id)+'&sum='+str(sum))
+    res = fp.read()
 
     return json.loads(res)
