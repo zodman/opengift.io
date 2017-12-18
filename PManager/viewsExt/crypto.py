054fb8bdcd9efa74789c51caf8772ad30f56e03c
gvammer <gvamm3r@gmail.com>
2017-12-18 23:13:41 +0300

    rating and likes

--- PManager/viewsExt/crypto.py
+++ PManager/viewsExt/crypto.py
@@ -1,12 +1,12 @@
 __author__ = 'Gvammer'
 import urllib, urllib2, json
-CRYPTO_HOST = 'http://'
+CRYPTO_HOST = '188.166.237.19'
 
 def bitcoin_set_request(project_id, sum):
-    service_url = '/bitcoin/request/create?project='+str(project_id)+'&sum='+str(sum)
+    service_url = '/bitcoin/request/create'
 
     url = "http://" + CRYPTO_HOST + service_url
-    req = urllib2.Request(url, '')
+    req = urllib2.Request(url, '?project='+str(project_id)+'&sum='+str(sum))
     result = urllib2.urlopen(req)
     res = result.read()
 
