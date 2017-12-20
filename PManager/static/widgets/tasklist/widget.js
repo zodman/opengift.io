2146948caaaff2b293224386413044d6e6b672c0
gvammer <gvamm3r@gmail.com>
2017-12-20 03:10:11 +0300

    rating and likes

--- PManager/static/widgets/tasklist/widget.js
+++ PManager/static/widgets/tasklist/widget.js
@@ -83,10 +83,10 @@ var widget_tl, currentGroup;
             '$movedTask': false,
             'TL_TaskTemplates': {},
             'TL_Tags': {
-                'Responsible': 'для ',
-                'Date': ' до ',
-                'Author': ' от ',
-                'About': 'примерно '
+                'Responsible': ' for ',
+                'Date': ' deadline ',
+                'Author': ' author ',
+                'About': ' estimate '
             },
             'TL_HintBlocks': {
                 'Responsible': $('#TL_responsible_list'),
@@ -871,7 +871,7 @@ var widget_tl, currentGroup;
                     '<div class="task clearfix" ' + (group.closed ? 'style="color: green;" ' : '') +
                     'data-milestoneId="' + group.id + '">' +
                     (group.url ? '<a href="' + group.url + '" class="js-milestone-data">' : '<span class="js-milestone-data">') +
-                    group.name + (group.date ? ' до ' + group.date : '') + (group.url ? '</a>' : '</span>') +
+                    group.name + (group.date ? ' before ' + group.date : '') + (group.url ? '</a>' : '</span>') +
                     (group.date ? '<div class="pull-right milestone-icons">' +
                     (group.closed ? '<span style="display: inline-block; width: 47px;"></span>' : '') +
                     '<a href="#" class="fa fa-edit js-edit-milestone-link"' + (group.closed ? 'style="color: green;" ' : '') + 'data-toggle="modal" data-target="#edit-milestone" data-edit-id="' + group.id +
