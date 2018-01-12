0083003bf99fd59ed23a14731afc59dc1b306dd9
gvammer <gvamm3r@gmail.com>
2018-01-12 12:31:00 +0300

    Donate for the goal

--- PManager/viewsExt/public.py
+++ PManager/viewsExt/public.py
@@ -6,10 +6,16 @@ from django.template import loader, RequestContext
 class Public:
     @staticmethod
     def mainPage(request):
+        try:
+            project = PM_Project.objects.get(pk=365)
+        except PM_Project.DoesNotExist:
+            project = PM_Project.objects.all()[0]
+
         c = RequestContext(request, {
             'projects_qty': PM_Project.objects.filter(public=True).count(),
             'developers_qty': PM_User.objects.filter(blockchain_wallet__isnull=False).count(),
             'donations_qty': PM_Project_Donation.objects.count(),
+            'milestones': project.milestones.filter(closed=False, donated=False).order_by('date'),
             'w': request.GET.get('w', '')
         })
 
