2f5de0efa91bfaf7f77a05945a2e489454ec4d13
gvammer <gvamm3r@gmail.com>
2018-01-12 11:08:31 +0300

    Donate for the goal

--- PManager/viewsExt/public.py
+++ PManager/viewsExt/public.py
@@ -1,13 +1,15 @@
 __author__ = 'Gvammer'
 from django.shortcuts import HttpResponse
-from PManager.models import PM_User, PM_Project
+from PManager.models import PM_User, PM_Project, PM_Project_Donation
 from django.template import loader, RequestContext
+
 class Public:
     @staticmethod
     def mainPage(request):
         c = RequestContext(request, {
             'projects_qty': PM_Project.objects.filter(public=True).count(),
             'developers_qty': PM_User.objects.filter(blockchain_wallet__isnull=False).count(),
+            'donations_qty': PM_Project_Donation.objects.count(),
             'w': request.GET.get('w', '')
         })
 
