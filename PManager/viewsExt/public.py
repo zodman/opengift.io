70740309fa5a040128d0550b6379e6a3d4fd8647
gvammer <gvamm3r@gmail.com>
2018-01-15 17:05:06 +0300

    Donate for the goal

--- PManager/viewsExt/public.py
+++ PManager/viewsExt/public.py
@@ -1,6 +1,6 @@
 __author__ = 'Gvammer'
 from django.shortcuts import HttpResponse
-from PManager.models import PM_User, PM_Project, PM_Project_Donation
+from PManager.models import PM_User, PM_Project, PM_Project_Donation, PM_Task
 from django.template import loader, RequestContext
 
 class Public:
@@ -20,3 +20,21 @@ class Public:
         })
 
         return HttpResponse(loader.get_template('public/index.html').render(c))
+
+    @staticmethod
+    def icoPage(request):
+        try:
+            project = PM_Project.objects.get(pk=495)
+        except PM_Project.DoesNotExist:
+            project = PM_Project.objects.all()[0]
+
+        c = RequestContext(request, {
+            'tasksQty': PM_Task.objects.filter(closed=True).count(),
+            'projects_qty': PM_Project.objects.filter(public=True).count(),
+            'developers_qty': PM_User.objects.filter(blockchain_wallet__isnull=False).count(),
+            'donations_qty': PM_Project_Donation.objects.count(),
+            'milestones': project.milestones.filter(closed=False, donated=False).order_by('date'),
+            'w': request.GET.get('w', '')
+        })
+
+        return HttpResponse(loader.get_template('public/ico.html').render(c))
