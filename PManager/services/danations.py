b18cd33fedd0f1fba85f13ef9c04490883bebab6
gvammer <gvamm3r@gmail.com>
2018-01-30 18:48:28 +0300

    recaptcha service

--- PManager/services/danations.py
+++ PManager/services/danations.py
@@ -11,7 +11,17 @@ def donate(sum, project, user=None, milestone=None, exchangeUser=None, refUser=N
     if user and user.is_authenticated() and user.get_profile().hasRole(project):
         return False
 
-    res = blockchain_donate_request(exchangeUser if exchangeUser else user.username, project.blockchain_name, sum)
+    milestoneCode = None
+    if milestone:
+        milestoneCode = 'opengift.io:' + milestone.id
+
+    res = blockchain_donate_request(
+        exchangeUser if exchangeUser else user.username,
+        project.blockchain_name,
+        sum,
+        milestoneCode
+    )
+
     if res == 'ok':
         if exchangeUser:
             try:
