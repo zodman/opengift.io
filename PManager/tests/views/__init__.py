317ad8f3e77282da2c2043b5379dd131cbf788a8
Your Name <you@example.com>
2018-11-08 23:43:06 -0600

    django test plus

---
+++ PManager/tests/views/__init__.py
@@ -0,0 +1,12 @@
+from test_plus.test import TestCase
+from model_mommy import mommy
+from django.contrib.auth.models import User
+from PManager.models import PM_Project
+
+class ViewsTest(TestCase):
+    def setUp(self):
+        self.usr = mommy.make(User)
+        self.project = mommy.make(PM_Project)
+
+    def test_pub(self):
+        self.get_check_200('PManager.viewsExt.public.Public.MainPage')
